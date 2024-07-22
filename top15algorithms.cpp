42b6d89f3747280f99d3dcb32af8aa25a2415df5
zhongzisha <zhongzisha@outlook.com>
2024-07-21 21:42:49 -0400

    top 15 algorithm patterns

---
+++ top15algorithms.cpp
@@ -0,0 +1,43 @@
+// prefix sum
+
+// two pointers
+
+// sliding window
+
+
+// fast & slow pointers
+
+// linkedlist in-place reversal
+
+// monotonic stack
+
+// top-k elements
+
+// overlapping intervals
+
+// modified binary search
+
+// binary tree traversal
+
+// depth-first search (DFS)
+
+// Breadth-first search (BFS)
+
+// matrix traversal
+
+// backtracking
+
+// dynamic programming patterns
+
+
+
+
+
+
+
+
+
+
+
+
+
